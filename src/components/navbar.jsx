057e9dcc7d4884e2d80ba5b94cab5f2983905a41
Demian Pulgar <dpulgar@Diegos-MacBook-Pro.local>
2025-10-08 13:36:20 -0300

    Incorporacion del NavBar en main.jsx e instalacion de bootstrap correctamente

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,12 +1,14 @@
 import '/src/style/style.css'
+import './Navbar.css'
+import logo from '../assets/Logo.png'
 
 function NavBar(){
     return(
-        <nav className="navbar navbar-expand-lg fixed-top">
+       <nav className="navbar navbar-expand-lg fixed-top">
         <div className="container-fluid">
-            <a className="navbar-brand" href="main.html">
-                <img src="./src/assets/Logo.png" alt="CodeGodot Logo" className="d-none d-md-block" style={{height: "60px", width: "auto"}} />
-                <img src="./src/assets/Logo.png" alt="CodeGodot Logo" className="d-md-none" style={{height: "40px", width: "auto"}} />
+            <a className="navbar-brand" href="/">
+                <img src={logo} alt="CodeGodot Logo" className="d-none d-md-block" style={{height: '60px', width: 'auto'}} />
+                <img src={logo} alt="CodeGodot Logo" className="d-md-none" style={{height: '40px', width: 'auto'}} />
             </a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span className="navbar-toggler-icon"></span>
@@ -14,16 +16,16 @@ function NavBar(){
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav ms-auto">
                     <li className="nav-item">
-                        <a className="nav-link text-white" href="#">INICIO</a>
+                        <a className="nav-link text-white" href="/">INICIO</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link text-white" href="#">COMUNIDAD</a>
+                        <a className="nav-link text-white" href="/comunidad">COMUNIDAD</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link text-white" href="#">INICIO SESIÓN</a>
+                        <a className="nav-link text-white" href="/login">INICIO SESIÓN</a>
                     </li>
                     <li className="nav-item">
-                        <a className="btn btn-light fw-bold ms-2 px-3" href="#" style={{color: "#1a5490"}}>REGISTRAR</a>
+                        <a className="btn btn-light fw-bold ms-2 px-3" href="/registro" style={{color: '#1a5490'}}>REGISTRAR</a>
                     </li>
                 </ul>
             </div>
@@ -34,6 +36,4 @@ function NavBar(){
 
 export default NavBar
 
-import 'bootstrap/dist/css/bootstrap.min.css'
-import NavBar from './components/Navbar'
 
