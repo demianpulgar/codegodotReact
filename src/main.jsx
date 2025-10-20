577e056d51c9606559c48471a0c5f0a695d22e41
demianpulgar <de.pulgar@duocuc.cl>
2025-10-20 18:43:34 -0300

    feat: agregar estilos CSS, Google Fonts y Font Awesome

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,16 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import './index.css'
+import '/src/style/style.css'
 import Navbar from './components/Navbar'
+import Inicio from './components/Inicio'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path="/" element={<div>Inicio</div>} />
+        <Route path="/" element={<Inicio />} />
         <Route path="/comunidad" element={<div>Comunidad</div>} />
         <Route path="/login" element={<div>Login</div>} />
         <Route path="/registro" element={<div>Registro</div>} />
