45ea36ea5fb5602b9bc1bec4892753c134e81fa6
demianpulgar <de.pulgar@duocuc.cl>
2025-10-22 17:36:48 -0300

    refactor: eliminar footer duplicado de Inicio - usar Footer global

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -127,58 +127,6 @@ function Inicio() {
                     </div>
                 </div>
             </section>
-
-            {/* Footer */}
-            <footer className="py-5">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-3 mb-4">
-                            <img src="/src/assets/Logo.png" alt="CodeGodot Logo" style={{height: '50px', filter: 'brightness(0) invert(1)'}} />
-                            <p className="mt-3" style={{fontSize: '0.9em'}}>Tu fuente para obtener codigo optimizado de godot y ahorrar tu tiempo :)</p>
-                            <div className="d-flex">
-                                <a href="#" className="text-white me-3">
-                                    <i className="fab fa-instagram" style={{fontSize: '24px'}}></i>
-                                </a>
-                                <a href="#" className="text-white me-3">
-                                    <i className="fab fa-twitter" style={{fontSize: '24px'}}></i>
-                                </a>
-                                <a href="#" className="text-white me-3">
-                                    <i className="fab fa-reddit" style={{fontSize: '24px'}}></i>
-                                </a>
-                                <a href="#" className="text-white">
-                                    <i className="fab fa-discord" style={{fontSize: '24px'}}></i>
-                                </a>
-                            </div>
-                        </div>
-                        <div className="col-lg-2 mb-4">
-                            <h6 className="fw-bold mb-3">Enlaces</h6>
-                            <ul className="list-unstyled">
-                                <li><a href="#" className="text-white-50 text-decoration-none">Inicio</a></li>
-                                <li><a href="#" className="text-white-50 text-decoration-none">Sobre nosotros</a></li>
-                                <li><a href="#" className="text-white-50 text-decoration-none">Servicios</a></li>
-                                <li><a href="#" className="text-white-50 text-decoration-none">Contacto</a></li>
-                                <li><a href="#" className="text-white-50 text-decoration-none">Comunidad</a></li>
-                            </ul>
-                        </div>
-                        <div className="col-lg-3 mb-4">
-                            <h6 className="fw-bold mb-3">Contacto y soporte</h6>
-                            <p style={{fontSize: '0.9em'}}>+56 9 0710 2001<br/>supportcodegodot@codegodot.cl<br/>Avenida Peruano, Tokyio, Indonesia</p>
-                        </div>
-                    </div>
-                    <hr className="my-4" />
-                    <div className="row">
-                        <div className="col-lg-6">
-                            <p className="mb-0" style={{fontSize: '0.9em'}}>© 2025 CodeGodot. All rights reserved.</p>
-                        </div>
-                        <div className="col-lg-6 text-lg-end">
-                            <a href="#" className="text-white-50 text-decoration-none me-3">Privacy Policy</a>
-                            <a href="#" className="text-white-50 text-decoration-none me-3">Terms of Service</a>
-                            <a href="#" className="text-white-50 text-decoration-none">Cookie Policy</a>
-                            <a href="#" className="text-white-50 text-decoration-none">Accessibility</a>
-                        </div>
-                    </div>
-                </div>
-            </footer>
         </>
     )
 }
